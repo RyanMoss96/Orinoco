7a91f7b01e6aafb8631f85f1065dd80725ef67c7
Chrysanthos Prodromou <pchrysanthos@gmail.com>
2016-03-13 13:35:46 +0000

    Initial admin interface

---
+++ admin/sidebar.php
@@ -0,0 +1,30 @@
+        <div class="col-sm-3 col-md-2 sidebar">
+          
+          <!-- Admin image & info -->
+          <div class="row placeholders">
+            <div class="col-xs-6 col-sm-3 placeholder" style="margin: 0 auto;width:100%">
+              <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
+              <h4>Chrysanthos</h4>
+              <span class="text-muted">Admin</span>
+            </div>
+          </div>
+
+          <ul class="nav nav-sidebar">
+            <li class="active"><a href="./index.php">Overview <span class="sr-only">(current)</span></a></li>
+            <li><a href="./products.php">Catalogue</a></li>
+            <li><a href="#">Analytics</a></li>
+            <li><a href="#">Export</a></li>
+          </ul>
+          <ul class="nav nav-sidebar">
+            <li><a href="">Nav item</a></li>
+            <li><a href="">Nav item again</a></li>
+            <li><a href="">One more nav</a></li>
+            <li><a href="">Another nav item</a></li>
+            <li><a href="">More navigation</a></li>
+          </ul>
+          <ul class="nav nav-sidebar">
+            <li><a href="">Nav item again</a></li>
+            <li><a href="">One more nav</a></li>
+            <li><a href="">Another nav item</a></li>
+          </ul>
+        </div>
