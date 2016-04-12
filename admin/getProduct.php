567dccb06bca8a9acc94777bd7b7a60e71fda1d1
Chrysanthos Prodromou <pchrysanthos@gmail.com>
2016-04-12 11:03:29 +0100

    Fixed edit page

--- admin/getProduct.php
+++ admin/getProduct.php
@@ -1,14 +1,15 @@
  <?php
-    require_once('db.php');
+require_once 'db.php';
 
-    function getProduct($id, $conn){
-        $sql="SELECT * FROM Products WHERE product_id=".$id;
-        if ($result = $conn->query($sql)) {
-            while($book = $result->fetch_object()){
-              return $book;
-            }
+function getProduct($id, $conn)
+{
+    $sql = "SELECT * FROM products WHERE product_id=" . $id;
+    if ($result = $conn->query($sql)) {
+        while ($book = $result->fetch_object()) {
+            return $book;
         }
-        return NULL;
     }
+    return null;
+}
 
 ?>
