354e4b216a4000a7623e904f3844587cc148211e
Chrysanthos Prodromou <pchrysanthos@gmail.com>
2016-04-17 20:58:11 +0100

    Products page now shows the products instead of lorem ipsum, Pagination still needed

--- admin/products.php
+++ admin/products.php
@@ -38,131 +38,55 @@
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
           <h1 class="page-header">Dashboard</h1>
 
-          <h2 class="sub-header">Section title</h2>
           <div class="table-responsive">
             <table class="table table-striped">
               <thead>
                 <tr>
                   <th>#</th>
-                  <th>Header</th>
-                  <th>Header</th>
-                  <th>Header</th>
-                  <th>Header</th>
+                  <th>Title</th>
+                  <th>Photo</th>
+                  <th>Description</th>
+                  <th>Price</th>
+                  <th>Discount price</th>
+                  <th>Quantity</th>
+                  <th>Actions</th>
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>1,001</td>
-                  <td>Lorem</td>
-                  <td>ipsum</td>
-                  <td>dolor</td>
-                  <td>sit</td>
-                </tr>
-                <tr>
-                  <td>1,002</td>
-                  <td>amet</td>
-                  <td>consectetur</td>
-                  <td>adipiscing</td>
-                  <td>elit</td>
-                </tr>
-                <tr>
-                  <td>1,003</td>
-                  <td>Integer</td>
-                  <td>nec</td>
-                  <td>odio</td>
-                  <td>Praesent</td>
-                </tr>
-                <tr>
-                  <td>1,003</td>
-                  <td>libero</td>
-                  <td>Sed</td>
-                  <td>cursus</td>
-                  <td>ante</td>
-                </tr>
-                <tr>
-                  <td>1,004</td>
-                  <td>dapibus</td>
-                  <td>diam</td>
-                  <td>Sed</td>
-                  <td>nisi</td>
-                </tr>
-                <tr>
-                  <td>1,005</td>
-                  <td>Nulla</td>
-                  <td>quis</td>
-                  <td>sem</td>
-                  <td>at</td>
-                </tr>
-                <tr>
-                  <td>1,006</td>
-                  <td>nibh</td>
-                  <td>elementum</td>
-                  <td>imperdiet</td>
-                  <td>Duis</td>
-                </tr>
-                <tr>
-                  <td>1,007</td>
-                  <td>sagittis</td>
-                  <td>ipsum</td>
-                  <td>Praesent</td>
-                  <td>mauris</td>
-                </tr>
-                <tr>
-                  <td>1,008</td>
-                  <td>Fusce</td>
-                  <td>nec</td>
-                  <td>tellus</td>
-                  <td>sed</td>
-                </tr>
-                <tr>
-                  <td>1,009</td>
-                  <td>augue</td>
-                  <td>semper</td>
-                  <td>porta</td>
-                  <td>Mauris</td>
-                </tr>
-                <tr>
-                  <td>1,010</td>
-                  <td>massa</td>
-                  <td>Vestibulum</td>
-                  <td>lacinia</td>
-                  <td>arcu</td>
-                </tr>
-                <tr>
-                  <td>1,011</td>
-                  <td>eget</td>
-                  <td>nulla</td>
-                  <td>Class</td>
-                  <td>aptent</td>
-                </tr>
-                <tr>
-                  <td>1,012</td>
-                  <td>taciti</td>
-                  <td>sociosqu</td>
-                  <td>ad</td>
-                  <td>litora</td>
-                </tr>
-                <tr>
-                  <td>1,013</td>
-                  <td>torquent</td>
-                  <td>per</td>
-                  <td>conubia</td>
-                  <td>nostra</td>
-                </tr>
-                <tr>
-                  <td>1,014</td>
-                  <td>per</td>
-                  <td>inceptos</td>
-                  <td>himenaeos</td>
-                  <td>Curabitur</td>
-                </tr>
-                <tr>
-                  <td>1,015</td>
-                  <td>sodales</td>
-                  <td>ligula</td>
-                  <td>in</td>
-                  <td>libero</td>
-                </tr>
+                <?php
+
+/* Select queries return a resultset */
+$sql = "SELECT * FROM products";
+$result = $conn->query($sql);
+// var_dump($result);
+// trigger_error($conn->error . " " . $sql);
+if ($result !== false) {
+    while ($book = $result->fetch_object()) {
+        echo "<tr>";
+        echo "<td>" . $book->product_id . "</td>";
+        echo "<td>" . $book->title . "</td>";
+        echo "<td>"
+        . "<a href='" . $book->photo_url . "' data-lightbox='image-1'>"
+        . "<img height='50px' src='" . $book->photo_url . "' data-lightbox='image-1'/></a></td>";
+        echo "<td>" . $book->description . "</td>";
+        echo "<td>" . $book->price . "</td>";
+        echo "<td>" . $book->discount_price . "</td>";
+        echo "<td>" . $book->quantity . "</td>";
+        // echo "<td> "
+        // ."<a href='./index.php?action=delete&id=". $book->product_id ."' class='btn btn-danger'>X</a>";
+        echo "<td>"
+        . "<form action =\"#\" method=\"POST\">
+                          <a href='./index.php?action=edit&id=" . $book->product_id . "' class='btn btn-success'>Edit</a>
+                          <a href='./index.php?action=delete&id=" . $book->product_id . "' class='btn btn-danger' >X</button>
+                          </form>"
+            . "</td>";
+
+        echo "</tr>";
+    }
+}
+
+?>
+
               </tbody>
             </table>
           </div>
